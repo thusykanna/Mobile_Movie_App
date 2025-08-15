bb803a9fd64e248925b8ef770662e7833f12a10f
thusykanna <thusy0816@gmail.com>
2025-08-15 20:22:09 +0530

    Fix import case for SearchBar, update dependencies, and adjust TypeScript config

--- Movies_App/app/(tabs)/index.tsx
+++ Movies_App/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import SearchBar from "@/components/searchBar";
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Link } from "expo-router";
@@ -11,7 +11,7 @@ export default function Index() {
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
         <View className="flex-1 mt-5">
-          <SearchBar />
+        <SearchBar />
         </View>
       </ScrollView>
     </View>
