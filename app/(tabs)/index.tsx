b3cc71b1844f8159346468a26a7f5fac42b2761b
thusykanna <thusy0816@gmail.com>
2025-08-16 19:25:27 +0530

    Update .gitignore, enhance SearchBar component, and modify Index screen for search functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,15 +3,21 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Link } from "expo-router";
 import {Image, ScrollView, Text, View } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0"/>
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
         <View className="flex-1 mt-5">
-        <SearchBar />
+        <SearchBar
+          placeholder="Search movies..."
+          onPress={() => router.push("/search")}
+        />
         </View>
       </ScrollView>
     </View>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,21 +1,31 @@
-import { View, Text, Image, TextInput } from 'react-native'
-import React from 'react'
-import { icons } from '@/constants/icons'
+import { View, Text, Image, TextInput } from "react-native";
+import React from "react";
+import { icons } from "@/constants/icons";
 
-const SearchBar = () => {
+interface Props {
+  placeholder?: string;
+  onPress?: () => void;
+}
+
+const SearchBar: React.FC<Props> = ({ placeholder, onPress }) => {     //Code to be reconsidered
   return (
-    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
-      <Image source={icons.search} className="w-5 h-5" resizeMode="contain" style = {{tintColor: "#ab8bff"}} />
-      <TextInput 
-        placeholder='Search'
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="w-5 h-5"
+        resizeMode="contain"
+        style={{ tintColor: "#ab8bff" }}
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
         placeholderTextColor="#a8b5db"
-        onPress={() => {}}
         value=""
         onChangeText={() => {}}
         className="flex-1 ml-2 text-white"
       />
     </View>
-  )
-}
+  );
+};
 
 export default SearchBar;
