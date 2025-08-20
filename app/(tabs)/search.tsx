495c79acb9ee5d2cb79a03296ad3f412e2eb1903
thusykanna <thusy0816@gmail.com>
2025-08-21 00:19:31 +0530

    Enhance search functionality by integrating SearchBar component and implementing loading and error handling for movie fetching

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,31 +1,84 @@
-import { View, Text, Image, FlatList } from 'react-native'
-import React from 'react'
-import { images } from '@/constants/images'
-import MovieCard from '@/components/MovieCard'
-import { fetchPopularMovies } from '@/services/api'
-import useFetch from '@/services/useFetch'
-import { useRouter } from 'expo-router'
+import { View, Text, Image, FlatList, ActivityIndicator } from "react-native";
+import React, { useState } from "react";
+import { images } from "@/constants/images";
+import MovieCard from "@/components/MovieCard";
+import { fetchPopularMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { icons } from "@/constants/icons";
+import SearchBar from "@/components/SearchBar";
 
 const search = () => {
-  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState("");
 
   const {
-    data : movies, 
-    loading : moviesLoading, 
-    error : moviesError} = useFetch(() =>
-    fetchPopularMovies({query : ''
-  }));
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+    reFetch: refetchMovies
+  } = useFetch(() => fetchPopularMovies({ query: searchQuery }), false);
 
   return (
-    <View className='flex-1 bg-primary'>
-      <Image source = {images.bg} className='flex-1 absolute w-full z-0' resizeMode='cover' />
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
       <FlatList
         data={Array.isArray(movies) ? movies : []}
-        renderItem={({item}) => <MovieCard {...item} />}
-        keyExtractor={(item) => item.id?.toString?.() ?? item.slug ?? item.title ?? Math.random().toString()}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) =>
+          item.id?.toString?.() ??
+          item.slug ??
+          item.title ??
+          Math.random().toString()
+        }
+        className="px-5"
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row justify-center mt-20 items-center">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar
+                placeholder="Search movies..."
+                value={searchQuery}
+                onChangeText={setSearchQuery}
+                onSubmitEditing={refetchMovies}
+              />
+            </View>
+
+            {moviesLoading && (
+              <ActivityIndicator size="large" color="#0000ff" className="my-3"/>
+            )}
+
+            {moviesError && (
+              <Text className="text-red-500 px-5 my-3">
+                Error: {moviesError?.message || "Failed to load movies"}
+              </Text>
+            )}
+
+            {!moviesLoading && !moviesError && searchQuery.trim() && movies?.length > 0 && (
+              <Text className="text-xl text-white font-bold">
+                Search Results for{' '}
+                 <Text className="text-accent">{searchQuery}</Text>
+              </Text>
+            )}
+
+          </>
+        }
       />
     </View>
-  )
-}
+  );
+};
 
-export default search
+export default search;
